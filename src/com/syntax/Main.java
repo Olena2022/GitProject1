96074e72024f571b0b637b6b8799bb12bb0a9df1
Olena <ovp.florida@gmail.com>
2022-05-14 12:34:49 -0400

    3rd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("I am Groot!!!");
-        System.out.println("I am Groot2!!!");
-        System.out.println("I am Groot3!!!");
+        System.out.println("I am Groot 2!!!");
+        System.out.println("I am Groot 3!!!");
+
+        System.out.println("i am correct");
     }
 }
